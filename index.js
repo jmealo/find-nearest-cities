90f10f796b7ee153393487e90229ddf547161f75
Jeff Mealo <jeff.m@jarv.us>
2021-02-04 20:23:58 -0500

    fix: isFloat() fails on 0.0

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const geokdbush = require('geokdbush');
 // [ city, latitude, longitude, state ] ex: ["Manayunk", 40.02456, -75.21407, "PA"]
 const index = new KDBush(points, p => p[2], p => p[1], 64);
 
-const isFloat = (n) => Number(n) === n && n % 1 !== 0;
-const nearestCities = (latitude, longitude, maxDistance, maxResults =  5) => {
-    if (!isFloat(latitude)) throw new Error('`latitude` has to be a Float');
-    if (!isFloat(longitude)) throw new Error('`longitude` has to be a Float');
+const nearestCities = (latitude, longitude, maxDistance, maxResults =  5, warningsOff = false) => {
+    latitude = typeof latitude !== 'number' ? parseFloat(latitude) : latitude
+    longitude = typeof longitude !== 'number' ? parseFloat(longitude): longitude
+    if (isNaN(latitude) || isNaN(longitude)) {
+        process.emitWarning(`nearestCities: Invalid coordinates: ${latitude}, ${longitude}; returning empty set`);
+        return []
+    }
 
-    const maxDistanceInMeters = maxDistance ? maxDistance / 1000 : maxDistance;
+    const maxDistanceInMeters = typeof maxDistance === 'number' ? maxDistance / 1000 : maxDistance;
     return geokdbush.around(index, longitude, latitude, maxResults, maxDistanceInMeters).map(city => {
         const [name, lat, lon, state] = city;
         return {name, lat, lon, state}
